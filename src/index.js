341642fe6f1f21501f7cd09e3788072402472679
Max <133934232+Kleostro@users.noreply.github.com>
2023-08-29 00:49:31 +0300

    fix: fixed redundant check

--- src/index.js
+++ src/index.js
@@ -2,17 +2,23 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-    if (matrix === undefined ||matrix.length === 0) {
-        return [];
-    } else {
-        let arr = [];
-        for (let i = 0; i < matrix.length; i++) {
-          if (i % 2 === 0) {
-            arr.push(...matrix[i]);
-          } else {
-            arr.push(...matrix[i].reverse());
-          };
-        };
-        return arr;
-    }
+    
+    if (matrix === undefined) return [];
+        
+    else {
+        
+    let arr = [];
+    
+    for (let i = 0; i < matrix.length; i++) {
+    
+      if (i % 2 === 0) arr.push(...matrix[i]);
+    
+      else arr.push(...matrix[i].reverse());
+    
+    };
+        
+    return arr;
+        
+}
+    
 }
